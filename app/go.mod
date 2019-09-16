cfc15978c25f396a5ba9d59886a11602b704ed06
JUN SAKATA <jun.bj141400@gmail.com>
2019-09-16 20:22:38 +0900

    init

---
+++ app/go.mod
@@ -0,0 +1,8 @@
+module github.com/sakajunquality/devfest-indore-2019-demo/app
+
+go 1.13
+
+require (
+	github.com/go-chi/chi v4.0.2+incompatible
+	golang.org/x/net v0.0.0-20190912160710-24e19bdeb0f2 // indirect
+)
---
+++ app/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+
+	message := os.Getenv("MY_MESSAGE")
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(message))
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
